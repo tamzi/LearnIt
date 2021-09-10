1cb2918a608c0b094a59a52151f82e3dedb5440d
tamzi <tamrefrank@gmail.com>
2021-09-09 15:05:00 -1000

    added indigo, minimalist and YebluTheme in dark and light modes

--- app/src/main/java/com/tamzi/learnit/ui/theme/Color.kt
+++ app/src/main/java/com/tamzi/learnit/ui/theme/Color.kt
@@ -1,8 +1,63 @@
 package com.tamzi.learnit.ui.theme
 
+import androidx.compose.material.Colors
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.compositeOver
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+
+/*
+    We set out the various theme colours for our shades of dark and light theme
+    We will modify these
+
+*/
+
+
+/*Theme 1: IndigoTheme:*/
+val indigo = Color(0xFF654D67)
+val lightIndigo = Color(0xFF9D5875)
+val lighterIndigo = Color(0xFFE5ABB2)
+val darkIndigo = Color(0xFF312651)
+
+/*Theme 2*/
+val white = Color(0xFFFFFFFF)
+val cream = Color(0xFFF6EDE2)
+val black = Color(0xFF131313)
+
+/*Theme 3: MinimalistTheme*/
+val gray = Color(0xFFEFEFEF)
+val deepGray = Color(0xFFE6E4E6)
+val darkGray = Color( 0xFF83829A)
+val lightGray = Color(0xFFF6F6F6)
+
+/*
+Redddish theme
+*/
+val reddish = Color(0xFFFF4343)
+val warmRed = Color(0xFFFF7754)
+val alilReddish = Color(0xffff0266)
+val darkerReddish = Color(0xffd8004d)
+val darkReddish = Color(0xff24191c)
+
+
+val blueish = Color(0xff0336ff)
+val darkerBlue = Color(0xff0035c9)
+val blueDarkPrimary = Color(0xff1c1d24)
+
+/*the yellow*/
+val lightYellow = Color(0xffffeb46)
+val yellowish = Color(0xffffc000)
+val darkYellow = Color(0xffffde03)
+val yellowDarkPrimary = Color(0xff242316)
+val limeGreenish = Color(0xFF4ABB00)
+
+
+
+/**
+ * Return the fully opaque color that results from compositing [onSurface] atop [surface] with the
+ * given [alpha]. Useful for situations where semi-transparent colors are undesirable.
+ */
+@Composable
+fun Colors.compositedOnSurface(alpha: Float): Color {
+    return onSurface.copy(alpha = alpha).compositeOver(surface)
+}
--- app/src/main/java/com/tamzi/learnit/ui/theme/Theme.kt
+++ app/src/main/java/com/tamzi/learnit/ui/theme/Theme.kt
@@ -1,44 +1,156 @@
 package com.tamzi.learnit.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+
+
+private val YebluThemeLight = lightColors(
+    primary = darkYellow ,
+    primaryVariant = darkYellow ,
+    onPrimary = black,
+    secondary = blueish,
+    secondaryVariant = darkerBlue,
+    onSecondary = white
 )
 
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+private val YebluThemeDark = darkColors(
+    primary = lightYellow,
+    secondary = indigo,
+    onSecondary = black,
+    surface = yellowDarkPrimary
 )
 
 @Composable
-fun LearnItTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun YebluTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
     val colors = if (darkTheme) {
-        DarkColorPalette
+        YebluThemeDark
     } else {
-        LightColorPalette
+        YebluThemeLight
     }
+    LearnItTheme(darkTheme, colors, content)
+}
 
-    MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
-        content = content
-    )
+private val IndigoThemeLight = lightColors(
+    primary = indigo,
+    primaryVariant = lightIndigo,
+    onPrimary = black,
+    secondary = lighterIndigo,
+    secondaryVariant = darkIndigo,
+    onSecondary = white,
+)
+private val IndigoThemeDark = lightColors(
+    primary = gray,
+    secondary = cream,
+    onSecondary = white,
+    surface = darkGray,
+)
+
+@Composable
+fun IndigoTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        IndigoThemeDark
+    } else {
+        IndigoThemeLight
+    }
+    LearnItTheme(darkTheme, colors, content)
+}
+
+private val RedThemeLight = lightColors(
+    primary = warmRed,
+    secondary = warmRed,
+    primaryVariant = darkerReddish,
+    onPrimary = Color.Black,
+    onSecondary = Color.Black
+)
+
+private val RedThemeDark = darkColors(
+    primary = reddish,
+    secondary = alilReddish,
+    surface = darkReddish
+)
+
+@Composable
+fun RedTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        RedThemeDark
+    } else {
+        RedThemeLight
+    }
+    LearnItTheme(darkTheme, colors, content)
+}
+
+
+
+private val MinimalistThemeLight = lightColors(
+    primary = gray,
+    secondary = darkGray,
+    primaryVariant = darkGray,
+    onPrimary = black,
+    onSecondary = black
+)
+
+private val MinimalistThemeDark = darkColors(
+    primary = gray,
+    secondary = lightGray,
+    surface = black
+)
+
+@Composable
+fun MinimalistTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        MinimalistThemeDark
+    } else {
+        MinimalistThemeLight
+    }
+    LearnItTheme(darkTheme, colors, content)
+}
+
+
+
+/*
+    LearnItTheme:
+    This is a custom theme but is extended from material design.
+
+    It cosnsits of: mimimalist,REd and
+ */
+@Composable
+private fun LearnItTheme(
+    darkTheme: Boolean,
+    colors: Colors,
+    content: @Composable () -> Unit
+) {
+
+}
+
+
+object LearnItTheme {
+
+    val colors: Colors
+        @Composable
+        get() = MaterialTheme.colors
+
+
+    val typography: Typography
+        @Composable
+        get() = MaterialTheme.typography
+
+    val shapes: Shapes
+        @Composable
+        get() = MaterialTheme.shapes
+
 }
