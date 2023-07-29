6838f3014e1aa3a599b7975aebb31bcefd46f545
tamzi <tamrefrank@gmail.com>
2023-07-29 10:19:12 +0200

    updates to gradle

--- learnacatalog/build.gradle.kts
+++ learnacatalog/build.gradle.kts
@@ -41,7 +41,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.7"
+        kotlinCompilerExtensionVersion = "1.5.0"
     }
 
     packaging {
@@ -54,7 +54,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.compose.material3:material3:1.1.0")
+    implementation("androidx.compose.material3:material3:1.1.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.4.3")
 
     implementation(project(":learna"))
